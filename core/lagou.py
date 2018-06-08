566b5e2fc1c8d3c06b0b457436bc5ebb83594ce5
dustingyz <dustingyz@gmail.com>
2018-06-08 22:26:55 +0800

    src changed

--- core/lagou.py
+++ core/lagou.py
@@ -37,7 +37,7 @@ class LagouScript(object):
         self.debug_log(json_result, self.get_page_num.__name__)
         total_count = int(json.loads(json_result.decode('utf-8'))["content"]["positionResult"]["totalCount"])
         self.debug_log(total_count, self.get_page_num.__name__)
-        print('***本次搜索到%d个职位***' % total_count)
+        print(('本次搜索到%d个职位' % total_count).center(50, '〓'))
         pagenum = int(math.ceil(total_count / 15))
         return pagenum
 
@@ -46,10 +46,10 @@ class LagouScript(object):
         self.debug_log(keyword_url)
         # city_list = ['北京', '上海']
         city_list = ['长沙']
-        pardir_name = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+        pardir_name = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
         for city in city_list:
             self.debug_log('current city is: %s' % city)
-            print('正在保存{city}的职位'.format(city=city).center(50, '*'))
+            print('正在保存{city}的职位'.format(city=city).center(50, '〓'))
             city_url = str2url(city)
             url = ('https://www.lagou.com/jobs/positionAjax.json?px=default&city='
                    '{city_url}'
@@ -96,35 +96,42 @@ class LagouScript(object):
                 'X-HttpWatch-RID': '6479-10040',
                 'X-Requested-With': 'XMLHttpRequest'
             }
-            pagenum = self.get_page_num(url, key_word, headers)
+            page_num = self.get_page_num(url, key_word, headers)
             idx = 0
-            for i in range(pagenum):
-                if i == 0:
-                    values = {'first': 'true', 'pn': '1', 'kd': key_word}
-                    data = urllib.parse.urlencode(values).encode('utf-8')
-                else:
+
+            filename = '{city}_{key_word}'.format(city=city, key_word=key_word)
+            file_uri = os.path.pardir + os.path.sep + 'result' + os.path.sep + pardir_name + os.path.sep + city + os.path.sep + filename
+            name = os.path.pardir + os.path.sep + 'result' + os.path.sep + pardir_name
+            if not os.path.exists(name):
+                os.mkdir(name)
+            name += os.path.sep + city
+            if not os.path.exists(name):
+                os.mkdir(name)
+
+            with open(file_uri, 'wb') as f:
+                for i in range(page_num):
+                    # if i == 0:
+                    #     values = {'first': 'true', 'pn': '1', 'kd': key_word}
+                    #     data = urllib.parse.urlencode(values).encode('utf-8')
+                    # else:
                     values = {'first': 'true', 'pn': (i + 1), 'kd': key_word}
                     data = urllib.parse.urlencode(values).encode('utf-8')
-                req = request.Request(url, data, headers)
+                    req = request.Request(url, data, headers)
+                    res = request.urlopen(req).read()
 
-                res = request.urlopen(req)
-                filename = '{city}_{number}.txt'.format(city=city, number=idx)
-                file_uri = os.path.pardir + os.path.sep + 'result' + os.path.sep + pardir_name + os.path.sep + city + os.path.sep + filename
-                for line in res:
-                    print('***正在保存第%d页***' % (i + 1))
+                    print('正在保存第{page}页'.format(page=(i + 1)).center(50, '〓'))
                     if self.debug_flg == 2:
-                        print(line)
+                        print(res)
                     else:
-                        name = os.path.pardir + os.path.sep + 'result' + os.path.sep + pardir_name
-                        if not os.path.exists(name):
-                            os.mkdir(name)
-                        name += os.path.sep + city
-                        if not os.path.exists(name):
-                            os.mkdir(name)
-                        with open(file_uri, 'wb') as f:
-                            f.write(line)
+                        json_data = json.loads(res, encoding='utf-8')
 
-                idx += 1
+                        # with open(file_uri, 'wb') as f:
+                        #     f.write(res)
+
+                    idx += 1
+
+    def save_request_msg(self):
+        pass
 
     def debug_log(self, obj, method=''):
         if self.debug_flg:
@@ -137,6 +144,9 @@ if __name__ == "__main__":
         flg = 1
     elif len(sys.argv) > 1 and sys.argv[1] == 'debug-p':
         flg = 2
+    result_dir = os.path.pardir + os.path.sep + 'result'
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
     keyword = input('请输入要爬取的关键词：')
     script = LagouScript(flg)
     script.lagou_spider(keyword)
